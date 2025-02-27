ba73b414b369b5c68a54747a29fa3cb6b3bd9ca0
victalejo <victalejocj@gmail.com>
2025-02-27 16:12:55 -0500

    2025.2.27.6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,43 @@
 // app/layout.tsx
 
-// /app/layout.tsx
 import './globals.css';
 import type { Metadata } from 'next';
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
+import { CartProvider } from '@/context/cart-context'; // Importamos el CartProvider
 
 export const metadata: Metadata = {
-  title: 'Impulse Rentals - Premier Boat & Bounce House Rentals',
-  description: 'Your premier destination for boat and bounce house rentals. Create unforgettable memories with our top-quality equipment and exceptional service.',
+    title: 'Impulse Rentals - Premier Boat & Bounce House Rentals',
+    description: 'Your premier destination for boat and bounce house rentals. Create unforgettable memories with our top-quality equipment and exceptional service.',
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="es" suppressHydrationWarning>
-      <head>
-        <link rel="preload" href="/fonts/BebasNeue-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/AcuminProCond-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/edo.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-      </head>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          <main className="min-h-screen">{children}</main>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="es" suppressHydrationWarning>
+        <head>
+            <link rel="preload" href="/fonts/BebasNeue-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+            <link rel="preload" href="/fonts/AcuminProCond-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+            <link rel="preload" href="/fonts/edo.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        </head>
+        <body>
+        <CartProvider> {/* Envolvemos toda la aplicación con CartProvider */}
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="light"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <Navbar />
+                <main className="min-h-screen">{children}</main>
+                <Footer />
+            </ThemeProvider>
+        </CartProvider>
+        </body>
+        </html>
+    );
 }
